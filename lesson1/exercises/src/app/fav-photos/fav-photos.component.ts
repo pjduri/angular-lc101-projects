b3f6f9ec6087b913546177cc87ca373628364306
Your Name <pjduri@gmail.com>
2022-08-04 22:14:28 -0500

    Completed 'Modify CSS' section of exercise.

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'More Fire Pics';
+  image1 = "https://scontent-atl3-1.xx.fbcdn.net/v/t31.18172-8/981034_549251351779666_8409105_o.jpg?_nc_cat=102&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=dotegohIcmUAX_B2PkH&_nc_ht=scontent-atl3-1.xx&oh=00_AT88yBOjzoZvY4S0x0ToSL8rO-K8ZCVa0pHbe450-XjLjw&oe=63105510";
+  image2 = 'https://scontent-mia3-2.xx.fbcdn.net/v/t31.18172-8/1276212_10201914317243219_1394670898_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=oXk3aq5205UAX8fsXHM&_nc_ht=scontent-mia3-2.xx&oh=00_AT86nvw-1T8fyUBgMiSG9B8GIfpnljzLG_3RqeaQ9zGagQ&oe=631135F7';
+  image3 = 'https://scontent-atl3-1.xx.fbcdn.net/v/t31.18172-8/965830_550451611659640_744079158_o.jpg?_nc_cat=109&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=DwR4nCLaJKkAX_g5qKg&_nc_ht=scontent-atl3-1.xx&oh=00_AT_uWl7wlTpVT0G8YWoiyqs-SYNz_tJyOFcFSzHQ_ceJaw&oe=63112056';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = [
+      'The Manchurian Candidate', 
+      'Oceans 8', 
+      'Waking Life', 
+      'Reefer Madness: The Movie Musical',
+      "Jet Li's Fearless",
+      'Ex Machina'
+   ];
 
    constructor() { }
 
