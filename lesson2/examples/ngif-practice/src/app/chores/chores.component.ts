8b19650e302498005d2868f2d81956a1c577d667
Your Name <pjduri@gmail.com>
2022-08-06 00:47:21 -0500

    Set booleans to determine whether allowance and/or ice cream were awarded.

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = ['Complete LaunchCode prep work', 'Buy groceries', 'Empty trash'];
+   finishedChores = ['GA5', 'Empty dishwasher', 'Remember === instead of = for your numbers', 
+      'Add some items to finishedChores to make the page change', 'Need one more to get the target up', 'This should be the last one'];
 
-   targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-
+   // provided target image doesn't work
+   // targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+   targetImage = 'https://image.shutterstock.com/image-vector/shot-target-260nw-768017731.jpg'
+   
    constructor() { }
 
    ngOnInit() {
