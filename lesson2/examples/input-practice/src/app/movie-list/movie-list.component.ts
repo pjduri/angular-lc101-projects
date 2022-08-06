3faab88f7694c66972bd5bdd7d8a9c7d1ab0e873
Your Name <pjduri@gmail.com>
2022-08-06 02:34:06 -0500

    Added keyup and click events to modify movie list.  Stuck trying to make error message disappear after click event with valid input.

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,15 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie (newTitle: string) {
+
+      if(!this.movies.includes(newTitle) && newTitle !== '') {
+         this.movies.push(newTitle)
+         return 'ok'
+      } else {
+         return 'no'
+      }
+      
+   }
 }
