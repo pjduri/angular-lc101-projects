a81aff279622c260857a8992d2ecdceee11b139a
Your Name <pjduri@gmail.com>
2022-08-07 00:42:20 -0500

    Examples

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
+   jokeButtonHeading: string = "Joke Buttons"
    inactive: boolean = false;
+   buttonPressed: boolean[] = [false, false, false]
+   location: string = 'center'
 
    constructor() { }
 
    ngOnInit() { }
 
+   resetButtons () {
+      for (let i=0; i<this.buttonPressed.length; i++) {
+         this.buttonPressed[i] = false
+      }
+   }
+
+   moveButton (oldLocation: string) {
+      while (this.location === oldLocation) {
+         this.location = ['left', 'right', 'center'][Math.floor(Math.random() * 3)]
+      }
+      return this.location
+   }
+   
 }
+
--- lesson3/examples/src/app/skill-set/skill-set.component.ts
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillSetComponent implements OnInit {
    listHeading: string = 'Some Coding Skills I Know';
-   skills: string[] = ['Loops', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
+   skills: string[] = [
+      'Loops', 
+      'Conditionals', 
+      'Functions', 
+      'Classes', 
+      'Modules', 
+      'Git', 
+      'HTML/CSS'
+   ];
    alternateColor: string = 'black';
    bulletType: string = 'A';
    changeColor: boolean = true;
