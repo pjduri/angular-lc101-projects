ff9b1384053e5804af2988a0d97c22d59a87b867
Your Name <pjduri@gmail.com>
2022-08-08 03:27:38 -0500

    I messed with this one way too much lol. Directional buttons move the rocket appropriately and grey out when rocket is too close to edge.

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,48 +1,139 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Builder } from 'protractor'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Exercises: Angular Lesson 3';
+  title: string = 'Exercises: Angular Lesson 3'
+  color: string = 'green'
+  height: number = 0
+  width: number = 0
+  message: string = 'Space shuttle ready for takeoff!'
+  takeOffEnabled: boolean = true
+  disableLeftButton: boolean = false
+  disableRightButton: boolean = false
+  disableUpButton: boolean = false
+  disableDownButton: boolean = false
 
-  color = 'green';
-  height = 0;
-  width = 0;
-  message = 'Space shuttle ready for takeoff!';
-
-  handleTakeOff() {
+  handleTakeOff(rocket: { style: { bottom: string; left: string } }) {
+    
+    if (this.message === 'Shuttle in flight.') {
+      window.alert('Shuttle already in flight.')
+      return
+    }
+    this.takeOffEnabled = false
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?')
-    if (result) {
-      this.color = 'blue';
-      this.height = 10000;
-      this.width = 0;
-      this.message = 'Shuttle in flight.';
+    if (result && this.message !== 'Shuttle in flight.') {
+      this.color = 'blue'
+      this.height = 10000
+      this.width = 0
+      this.message = 'Shuttle in flight.'
+      rocket.style.left = '240px'
+      rocket.style.bottom = '10px'
+      return
     }
   }
   
-  handleLanding() {
-    let result = window.confirm('The shuttle is landing. Landing gear engaged.')
-    if (result) {
-      this.color = 'green';
-      this.height = 0;
-      this.width = 0;
-      this.message = 'Shuttle has landed.';
+  handleLanding(rocket: { style: { bottom: string; left: string } }) {
+    if (this.message !== 'Shuttle in flight.') {
+      window.alert('Shuttle is already on ground.')
+    } else {
+      this.takeOffEnabled = true
+      window.alert('The shuttle is landing. Landing gear engaged.')
+      this.color = 'green'
+      this.height = 0
+      this.width = 0
+      this.message = 'Shuttle has landed.'
+      rocket.style.bottom = '0px'
+      rocket.style.left = '0px'
     }
   }
 
-  handleAbort() {
-    let result = window.confirm('Are you sure you want to abort?')
-    if (result) {
-      this.color = 'red';
-      this.height = 0;
-      this.width = 0;
-      this.message = 'Mission aborted.';
+  handleAbort(rocket: { style: { bottom: string; left: string } }) {
+    if (this.message !== 'Shuttle in flight.') {
+      window.alert('Shuttle is already on ground.')
+    } else {
+      this.takeOffEnabled = true
+      let result = window.confirm('Are you sure you want to abort?')
+      if (result) {
+        this.color = 'red'
+        this.height = 0
+        this.width = 0
+        this.message = 'Mission aborted.'
+        rocket.style.bottom = '0px'
+        rocket.style.left = '0px'
+      }
     }
+  }
+
+  // { style: { left: string } } was a quick fix from vscode.  
+  // find out about it - probably 
+  moveRocket(rocket: { style: { left: string; bottom: string } }, direction: string) {
+    
+    if (this.message !== 'Shuttle in flight.') {
+      window.alert('Must take off first.')
+    } 
+    if (direction === 'right') {
+      rocket.style.left = parseInt(rocket.style.left) + 10 + 'px'
+    } 
+    if (direction === 'left') {
+      rocket.style.left = parseInt(rocket.style.left) - 10 + 'px'
+    } 
+    if (direction === 'up') {
+      this.height += 10000
+      rocket.style.bottom = parseInt(rocket.style.bottom) + 10 + 'px'
+    } 
+    if (direction === 'down') {
+        this.height -= 10000
+        rocket.style.bottom = parseInt(rocket.style.bottom) - 10 + 'px'
+    }
+
+    if (parseInt(rocket.style.left) < 0 && this.message === 'Shuttle in flight.') {
+      this.color = 'orange'
+      this.disableLeftButton = true
+    } else {
+      this.color = 'blue'
+      this.disableLeftButton = false
+    }
+    if (parseInt(rocket.style.left) > 460 && this.message === 'Shuttle in flight.') {
+      this.color = 'orange'
+      this.disableRightButton = true
+    } else {
+      this.color = 'blue'
+      this.disableRightButton = false
+    }
+    if (parseInt(rocket.style.bottom) > 300 && this.message === 'Shuttle in flight.') {
+      this.color = 'orange'
+      this.disableUpButton = true
+    } else {
+      this.color = 'blue'
+      this.disableUpButton = false
+    }
+    if (parseInt(rocket.style.bottom) < 10 && this.message === 'Shuttle in flight.') {
+      this.color = 'orange'
+      this.disableDownButton = true
+    } else {
+      this.color = 'blue'
+      this.disableDownButton = false
+    }
+
+    // if (parseInt(rocket.style.left) < 0) {
+    //   rocket.style.left = '480px'
+    // }
+    // if (parseInt(rocket.style.left) > 480) {
+    //   rocket.style.left = '0px'
+    // }
+    // if (parseInt(rocket.style.bottom) > 300) {
+    //   rocket.style.bottom = '0px'
+    // }
+    // if (parseInt(rocket.style.bottom) < 0) {
+    //   rocket.style.bottom = '300px'
+    // }
     
   }
-
 
 }
